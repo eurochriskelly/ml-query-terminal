bea0cc1f48ac3772b59ce7cbe1115ed2cb72720b
Christopher Kelly <chris.kelly@tahzoo.com>
2017-02-13 09:55:20 +0100

    add bin support

---
+++ bin/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const marklogic = require('marklogic');
+const CONFIG = require('../config.json');
+const db = marklogic.createDatabaseClient(CONFIG);
+
+const MLQC = require('../lib/ml-query-cli');
+const mlquerycli = new MLQC(db);
