9afaa76e86400949a5ae83ae851952c3541a7c23
Christopher Kelly <chris.kelly@tahzoo.com>
2017-03-24 18:52:53 +0100

    allow config in package.json. forward command line args to marklogic

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,25 @@
-'use strict';
+#!/usr/bin/env node
 
+'use strict';
+const path = require('path');
 const marklogic = require('marklogic');
-const CONFIG = require('../config.json');
+var CONFIG;
+try {
+    // load section of package.json
+    var pkg = require(path.resolve(process.cwd(), 'package.json'));
+    if (pkg.mlqt) {
+	CONFIG = pkg.mlqt;
+    }
+    else {
+	console.log('No config in package.json. Using defaults.');
+	CONFIG = require('../config.json');
+    }
+}
+catch (e) {
+    // load defaults
+    CONFIG = require('../config.json');
+}
+
 const db = marklogic.createDatabaseClient(CONFIG);
 
 const MLQC = require('../lib/ml-query-cli');
