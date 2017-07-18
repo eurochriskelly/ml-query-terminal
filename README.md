016fa0864f7a43a49e6d71200c698afd8ffe817b
Chris Kelly <chris.kelly@tahzoo.com>
2017-07-18 11:48:59 +0200

    pass variables to xquery

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Execute MarkLogic queries from the command line.
 
 ## Install
 
-    yarn add https://github.com/eurochriskelly/ml-query-terminal
+   npm add eurochriskelly/ml-query-terminal
     
 ## Setup
 
@@ -12,23 +12,25 @@ Execute MarkLogic queries from the command line.
 - Configure privileges for user in config.json (if not admin)
 - OPTIONAL: add scripts to package.json
 
+```
     "scripts" : {
       "mlqt" : "mlqt",
       "xq:setup" : "mlqt -q setup-file.xqy",
       "js:setup" : "mlqt -q setup.js",
     }
+```    
 
 ## Usage:
 
-   With yarn, adding to package.json scripts is not required by
+   With npm, adding to package.json scripts is not required by
    default. So you can just run:
 
-    yarn run mlqt -- -q examples/foo.xqy
+    npm run mlqt -- -q examples/foo.xqy
 
 Sends query to server, executes, and sends results back for parsing
 with command line tools.
 
-Useful for running simple setup commands from command line or running
+Useful for running custom build scripts from command line, or running
 on server where ssh is possible but http access to port 8000 is
 restricted.
 
@@ -39,4 +41,4 @@ every time the query changes.
 
 Usage:
 
-    yarn run mlqt -- -wq examples/foo.xqy
+    npm run mlqt -- -wq examples/foo.xqy
--- bin/index.js
+++ bin/index.js
@@ -1,29 +1,33 @@
 #!/usr/bin/env node
 
-'use strict';
-const path = require('path');
-const marklogic = require('marklogic');
-var sslRootCAs = require('ssl-root-cas/latest')
-sslRootCAs.inject()
+(function (){
+    'use strict';
 
-var CONFIG;
-try {
-    // load section of package.json
-    var pkg = require(path.resolve(process.cwd(), 'package.json'));
-    if (pkg.mlqt) {
-	CONFIG = pkg.mlqt;
+    const path = require('path');
+    const marklogic = require('marklogic');
+    var sslRootCAs = require('ssl-root-cas/latest')
+    sslRootCAs.inject()
+
+    var CONFIG;
+    try {
+        // load section of package.json
+        var pkg = require(path.resolve(process.cwd(), 'package.json'));
+        if (pkg.mlqt) {
+            CONFIG = pkg.mlqt;
+        }
+        else {
+            console.log('No config in package.json. Using defaults.');
+            loadDefaults()
+        }
     }
-    else {
-	console.log('No config in package.json. Using defaults.');
-	CONFIG = require('../config.json');
+    catch (e) { loadDefaults() }
+
+    const db = marklogic.createDatabaseClient(CONFIG);
+    const MLQC = require('../lib/ml-query-cli');
+    const mlquerycli = new MLQC(db);
+
+    function loadDefaults ()
+    {
+        CONFIG = require('../config.json');
     }
-}
-catch (e) {
-    // load defaults
-    CONFIG = require('../config.json');
-}
-
-const db = marklogic.createDatabaseClient(CONFIG);
-
-const MLQC = require('../lib/ml-query-cli');
-const mlquerycli = new MLQC(db);
+})();
